d2a75252fd8c8d0a0705ea5dba693236767da6e2
Vo Thanh Dat <vothanhdat@outlook.com>
2017-05-28 00:08:38 +0700

    Fix resize method

--- src/client/Object/GameScene/Scene.ts
+++ src/client/Object/GameScene/Scene.ts
@@ -40,6 +40,7 @@ class Scene extends PIXI.Container{
 
     resize(width : number,height : number){
         var min = Math.min(width,height)
+        this.position.set(width/2 - min/2,height/2 - min/2)
         this.scale.set(min / 10);
     }
 
